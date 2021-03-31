8346b6856710bbe84fad5479f51a003c79abf0ed
Jesse Wenger <wengerj@gmail.com>
2021-03-31 12:10:51 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -12,7 +12,7 @@ class Application
       item = @@items.find{|item| item.name == req_item}
       if item.nil?
         resp.write "Item not found"
-        resp.status = 404
+        resp.status = 400
       else
         resp.write item.price
         resp.status = 200
