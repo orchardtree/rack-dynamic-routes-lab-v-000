c0c6a6ac488b3ebac2cf0dcec9938473b29b0d7e
Jesse Wenger <wengerj@gmail.com>
2021-03-31 11:59:23 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
     @resp = Rack::Response.new
     @req = Rack::Request.new(env)
     
-    if req.path.match(/items/)
+    if @req.path.match(/items/)
       item_handler
     else
       resp.write "Route not found"
